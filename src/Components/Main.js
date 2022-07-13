52267603b9288833966d26e9f8d678d780194e04
saitejapadakandla <saitejapadakandla2011@gmail.com>
2022-07-13 14:05:14 -0700

    Test Commint

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,7 +9,7 @@ class Main extends Component {
       super()
       this.state = {posts:[{
         id: "0",
-        description: "beautiful landscape",
+        description: "Beautiful landscape",
         imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
         "3919321_1443393332_n.jpg"
         }, {

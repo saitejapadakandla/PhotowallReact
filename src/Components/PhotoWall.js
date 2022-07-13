eeeedf0c0202ad386a79ce6d69152bf38c3d7b44
saitejapadakandla <saitejapadakandla2011@gmail.com>
2022-07-13 13:57:31 -0700

    Init

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import Photo from './Photo'
+
+
+function Photowall(props){
+    return <div className="photoGrid">
+    {props.posts.map((post,index)=><Photo key={index}post={post}/>)}
+</div>
+}
+
+
+export default Photowall;
